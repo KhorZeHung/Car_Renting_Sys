a5e4ed294035fcd59845b0cd0f8016de0280b8ff
Khor Ze Hung <84367877+KhorZeHung@users.noreply.github.com>
2022-10-27 20:23:47 +0800

    Car (put & delete function)

--- back_end/util/router/cars.js
+++ back_end/util/router/cars.js
@@ -14,31 +14,68 @@ const db = mysql.createPool({
   database: env.DATABASE,
 });
 
-router.get("/", (req, res) => {
-  var query = `SELECT * FROM Car`;
-  db.query(query, (err, result) => {
-    if (err) {
-      res.sendStatus(400);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-router.post("/", (req, res) => {
-  var { Car_brand, Car_model, Car_rentPrice} =
-    req.body;
-  var query = `INSERT INTO Car (Car_brand, Car_model, Car_rentPrice) values (?, ?, ?)`;
-  db.query(query, [Car_brand, Car_model, Car_rentPrice], (err, result) => {
-    if(err){
-        console.log("fail")
+router
+  .route("/")
+  .get((req, res) => {
+    var query = `SELECT * FROM Car`;
+    db.query(query, (err, result) => {
+      if (err) {
         res.sendStatus(400);
-    }
-    else{
-        console.log("success" + result)
+      } else {
+        res.send(result);
+      }
+    });
+  })
+  .post((req, res) => {
+    var { Car_brand, Car_model, Car_rentPrice } = req.body;
+    var query = `INSERT INTO Car (Car_brand, Car_model, Car_rentPrice) values (?, ?, ?)`;
+    db.query(query, [Car_brand, Car_model, Car_rentPrice], (err, result) => {
+      if (err) {
+        res.sendStatus(400);
+      } else {
         res.sendStatus(200);
-    }
+      }
+    });
+  })
+  .put((req, res) => {
+    var { Car_id, Car_brand, Car_model, Car_rentPrice } = req.body;
+    var query = `UPDATE Car SET Car_brand = ?, Car_model = ?, Car_rentPrice = ? WHERE Car_id = ?`;
+    db.query(
+      query,
+      [Car_brand, Car_model, Car_rentPrice, Car_id],
+      (err, result) => {
+        if (err) {
+          res.sendStatus(400);
+        } else {
+          res.sendStatus(200);
+        }
+      }
+    );
   });
-});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    const carId = req.params.id;
+    const query = `SELECT * FROM Car WHERE Car_id = ?`;
+    db.query(query, [carId], (err, result) => {
+      if (err) {
+        res.sendStatus(400);
+      } else {
+        res.send(result);
+      }
+    });
+  })
+  .delete((req, res) => {
+    const carId = req.params.id;
+    const query = `DELETE FROM Car WHERE Car_id = ?`;
+    db.query(query, [carId], (err, result) => {
+      if (err) {
+        res.sendStatus(400);
+      } else {
+        res.send(result);
+      }
+    });
+  });
 
 module.exports = router;
